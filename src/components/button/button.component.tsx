7bf66bf466e44f7ab9c1c3212d3bd8efdb062cc9
Maryen <voujum@gmail.com>
2023-04-04 23:47:33 +0200

    Button component

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,20 @@
+import css from './button.module.css';
+
+const BUTTON_TYPE_CLASSES = {
+  basic: 'basic-button',
+  inverted: 'inverted-button',
+  accented: 'accented-button'
+};
+
+const Button = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button 
+      className={css['button-container'] + ' ' + css[BUTTON_TYPE_CLASSES[buttonType]]}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
