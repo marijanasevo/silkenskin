4628aa68868ddcd696551e1a1dfc19b1dc847cc4
Maryen <voujum@gmail.com>
2023-04-25 12:18:24 +0200

    Simple form control

--- src/components/product-review-form/product-review-form.component.tsx
+++ src/components/product-review-form/product-review-form.component.tsx
@@ -1,18 +1,20 @@
-import css from "./product-review-form.module.css";
-import TextField from "@mui/material/TextField";
-import { Rating } from "@mui/material";
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
-import Box from "@mui/material/Box";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { createReview, Product } from "../../utils/firebase/firebase.utils";
 import {
   selectCurrentUser,
   selectIsUserLoggedIn,
 } from "../../store/user/user.selector";
-import { useState } from "react";
-import { createReview, Product } from "../../utils/firebase/firebase.utils";
 import { fetchReviewsAsync } from "../../store/review/review.reducer";
+
+import TextField from "@mui/material/TextField";
+import FormControlUnstyled from "@mui/base/FormControlUnstyled";
+import { Rating, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { AppDispatch } from "../../store/store";
-import { useParams } from "react-router-dom";
+import css from "./product-review-form.module.css";
 
 type ReviewFieldsState = {
   rating: number | null;
@@ -33,6 +35,7 @@ const ProductReviewForm = () => {
   const { id: productID } = useParams() as { id: string };
   const [formFields, setFormFields] = useState(defaultReviewFields);
   const { rating, feedback, name } = formFields;
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -44,7 +47,26 @@ const ProductReviewForm = () => {
 
     let userName = currentUser ? currentUser.displayName : name;
 
-    if (!userName || !productID || !rating) return;
+    if (!rating || !feedback || !userName) {
+      const errorMessages = [];
+      if (!userName) errorMessages.push("enter your name");
+      if (!feedback) errorMessages.push("tell us about your experience");
+      if (!rating) errorMessages.push("select a rating");
+
+      setErrorMessage(
+        `We would love to hear from you but all fields are required. Please ` +
+          errorMessages.map((errorMessage, i) => {
+            return i === errorMessages.length - 1 && errorMessages.length > 1
+              ? " and also " + errorMessage
+              : " " + errorMessage;
+          }) +
+          "."
+      );
+
+      return;
+    }
+
+    setErrorMessage("");
 
     let product: Product = {
       userDisplayName: userName,
@@ -61,24 +83,76 @@ const ProductReviewForm = () => {
   };
 
   return (
-    <Box
-      component="form"
-      noValidate
-      autoComplete="off"
-      className={css["form-review"]}
-      sx={{
-        marginTop: "-3rem",
-      }}
-      onSubmit={submitReviewHandler}
-    >
-      {!isUserLoggedIn && (
+    <FormControlUnstyled required>
+      <Box
+        component="form"
+        autoComplete="off"
+        className={css["form-review"]}
+        sx={{
+          marginTop: "-3rem",
+        }}
+        onSubmit={submitReviewHandler}
+      >
+        {!isUserLoggedIn && (
+          <TextField
+            id="standard-textarea"
+            label="Your name"
+            variant="standard"
+            onChange={handleChange}
+            name="name"
+            value={name}
+            sx={{
+              "&.MuiTextField-root": {
+                margin: "0",
+              },
+              "& .Mui-focused.MuiInputLabel-root": {
+                color: "var(--strong-accent-color)",
+                fontSize: "1.9rem",
+                top: "-0.5rem",
+              },
+              "& .MuiInputLabel-shrink": {
+                top: "-0.5rem",
+                fontSize: "1.9rem",
+                color: "var(--strong-accent-color)",
+              },
+              "& .MuiInputLabel-root": {
+                fontSize: "1.5rem",
+                color: "var(--strong-accent-color)",
+              },
+              "& .MuiInputBase-root::before": {
+                borderBottom: "0 !important",
+              },
+              "& .MuiInputBase-input": {
+                fontSize: "1.4rem",
+                padding: "1rem",
+                borderBottom:
+                  "2px dashed var(--strong-accent-color) !important",
+                lineHeight: "130%",
+              },
+              "& .MuiInputBase-input.Mui-focused::before": {
+                fontSize: "1.4rem",
+                padding: "1rem",
+                borderBottom: "0",
+              },
+              "& .MuiInputBase-root.Mui-focused::after": {
+                fontSize: "1.4rem",
+                padding: "1rem",
+                borderBottom: "0",
+              },
+              "& .MuiInputBase-root-MuiInput-root:after": {
+                borderBottom: "0 !important",
+              },
+            }}
+          />
+        )}
         <TextField
           id="standard-textarea"
-          label="Your name"
+          label="Your experience"
+          multiline
           variant="standard"
           onChange={handleChange}
-          name="name"
-          value={name}
+          name="feedback"
+          value={feedback}
           sx={{
             "&.MuiTextField-root": {
               margin: "0",
@@ -100,99 +174,54 @@ const ProductReviewForm = () => {
             "& .MuiInputBase-root::before": {
               borderBottom: "0 !important",
             },
-            "& .MuiInputBase-input": {
+            "& .MuiInputBase-inputMultiline": {
               fontSize: "1.4rem",
               padding: "1rem",
               borderBottom: "2px dashed var(--strong-accent-color) !important",
               lineHeight: "130%",
             },
-            "& .MuiInputBase-input.Mui-focused::before": {
+            "& .MuiInputBase-multiline.Mui-focused::before": {
               fontSize: "1.4rem",
               padding: "1rem",
               borderBottom: "0",
             },
-            "& .MuiInputBase-root.Mui-focused::after": {
+            "& .MuiInputBase-multiline.Mui-focused::after": {
               fontSize: "1.4rem",
               padding: "1rem",
               borderBottom: "0",
             },
-            "& .MuiInputBase-root-MuiInput-root:after": {
+            "& .MuiInputBase-multiline:hover": {
               borderBottom: "0 !important",
+            },
+            "& .MuiInputBase-root::after": {
+              borderBottom: "0!important",
             },
           }}
         />
-      )}
-      <TextField
-        id="standard-textarea"
-        label="Your experience"
-        multiline
-        variant="standard"
-        onChange={handleChange}
-        name="feedback"
-        value={feedback}
-        sx={{
-          "&.MuiTextField-root": {
-            margin: "0",
-          },
-          "& .Mui-focused.MuiInputLabel-root": {
-            color: "var(--strong-accent-color)",
-            fontSize: "1.9rem",
-            top: "-0.5rem",
-          },
-          "& .MuiInputLabel-shrink": {
-            top: "-0.5rem",
-            fontSize: "1.9rem",
-            color: "var(--strong-accent-color)",
-          },
-          "& .MuiInputLabel-root": {
-            fontSize: "1.5rem",
-            color: "var(--strong-accent-color)",
-          },
-          "& .MuiInputBase-root::before": {
-            borderBottom: "0 !important",
-          },
-          "& .MuiInputBase-inputMultiline": {
-            fontSize: "1.4rem",
-            padding: "1rem",
-            borderBottom: "2px dashed var(--strong-accent-color) !important",
-            lineHeight: "130%",
-          },
-          "& .MuiInputBase-multiline.Mui-focused::before": {
-            fontSize: "1.4rem",
-            padding: "1rem",
-            borderBottom: "0",
-          },
-          "& .MuiInputBase-multiline.Mui-focused::after": {
-            fontSize: "1.4rem",
-            padding: "1rem",
-            borderBottom: "0",
-          },
-          "& .MuiInputBase-multiline:hover": {
-            borderBottom: "0 !important",
-          },
-          "& .MuiInputBase-root::after": {
-            borderBottom: "0!important",
-          },
-        }}
-      />
 
-      <Rating
-        sx={{
-          "& .MuiRating-icon": {
-            color: "#a89565",
-          },
-        }}
-        name="rating"
-        value={rating}
-        onChange={(event, newValue) => {
-          setFormFields({ ...formFields, rating: newValue });
-        }}
-        precision={0.5}
-        size="large"
-      />
+        <Rating
+          sx={{
+            "& .MuiRating-icon": {
+              color: "#a89565",
+            },
+          }}
+          name="rating"
+          value={rating}
+          onChange={(event, newValue) => {
+            setFormFields({ ...formFields, rating: newValue });
+          }}
+          precision={0.5}
+          size="large"
+        />
 
-      <Button buttonType={BUTTON_TYPE_CLASSES.basic}>Leave Feedback</Button>
-    </Box>
+        <Button buttonType={BUTTON_TYPE_CLASSES.basic}>Leave Feedback</Button>
+        {errorMessage && (
+          <Typography variant="caption" color="error">
+            {errorMessage}
+          </Typography>
+        )}
+      </Box>
+    </FormControlUnstyled>
   );
 };
 
