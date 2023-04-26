9d34fd2c6022cbeef1a90c441f7185a049306ec4
Maryen <voujum@gmail.com>
2023-04-26 20:07:51 +0200

    Order History got it's own compnent

---
+++ src/components/order-history/order-history.component.tsx
@@ -0,0 +1,91 @@
+import { useNavigate } from "react-router-dom";
+import css from "../../routes/account/account.module.css";
+import { Fragment, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
+import { Order } from "../../store/user/user.types";
+
+const OrderHistory = () => {
+  const navigate = useNavigate();
+  const currentUser = useSelector(selectCurrentUser);
+  const [purchases, setPurchases] = useState<Order[]>([]);
+  const [selectedOrderIndex, setSelectedOrderIndex] = useState(-1);
+
+  function toggleDescription(index: number) {
+    if (selectedOrderIndex === index) {
+      setSelectedOrderIndex(-1); // Deselect the order if it's already selected
+    } else {
+      setSelectedOrderIndex(index);
+    }
+  }
+
+  useEffect(() => {
+    if (currentUser) setPurchases(currentUser.purchases);
+  }, [currentUser]);
+
+  return (
+    <div className={css["order-table"]}>
+      <div className={css["order-table-header"]}>
+        <span>Order #</span>
+        <span>Total</span>
+        <span>Order Date</span>
+        <span>Items</span>
+        <span>Details</span>
+      </div>
+
+      {purchases.length
+        ? purchases.map((order, index) => {
+            const isSelected = selectedOrderIndex == index;
+
+            return (
+              <Fragment key={index}>
+                <div
+                  onClick={() => toggleDescription(index)}
+                  className={css["order-table-info"]}
+                >
+                  <span># {order.id.slice(14).toUpperCase()}</span>
+                  <span>${order.total}</span>
+                  <span>{new Date(order.createdAt).toLocaleDateString()}</span>
+                  <span>{order.products.length} items</span>
+                  <span>{isSelected ? "↑" : "↓"}</span>
+                </div>
+
+                <div
+                  className={`${css["order-details"]} ${
+                    isSelected ? css["show-details"] : ""
+                  }`}
+                >
+                  {order.products.map((product, index) => (
+                    <div key={index} className={css["order-item"]}>
+                      <img
+                        src={product.thumbnail}
+                        alt="item"
+                        className={css["order-item-thumbnail"]}
+                      />
+                      <div
+                        onClick={() => navigate(`/product/${product.id}`)}
+                        className={css["order-item-details"]}
+                      >
+                        <h3 className={css["order-item-title"]}>
+                          {product.name} <br />
+                          <span className={css["order-item-brand"]}>
+                            {product.brand}
+                          </span>
+                        </h3>
+                        <p>Quantity: {product.quantity}</p>
+                      </div>
+                      <div className={css["order-item-price"]}>
+                        <span>${product.price}</span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Fragment>
+            );
+          })
+        : "We're starting to think you're secretly a skincare expert - you haven't ordered anything from us yet"}
+    </div>
+  );
+};
+
+export default OrderHistory;
--- src/routes/account/account.component.tsx
+++ src/routes/account/account.component.tsx
@@ -1,48 +1,34 @@
-import { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectReviews } from "../../store/review/review.selector";
 
 import css from "./account.module.css";
 import { useEffect } from "react";
-import { Order } from "../../store/user/user.types";
+import OrderHistory from "../../components/order-history/order-history.component";
 
 const Account = () => {
   const navigate = useNavigate();
   const currentUser = useSelector(selectCurrentUser);
-  const [purchases, setPurchases] = useState<Order[]>([]);
-  const [selectedOrderIndex, setSelectedOrderIndex] = useState(-1);
-
-  function toggleDescription(index: number) {
-    if (selectedOrderIndex === index) {
-      setSelectedOrderIndex(-1); // Deselect the order if it's already selected
-    } else {
-      setSelectedOrderIndex(index);
-    }
-  }
 
   useEffect(() => {
-    if (!currentUser) {
-      navigate("/login");
-    } else {
-      setPurchases(currentUser.purchases);
-    }
+    if (!currentUser) navigate("/login");
   }, [currentUser]);
 
   const signOutHandler = async () => {
     await signOutUser();
-    navigate("/");
+    navigate("/login");
   };
 
   return (
     <>
-      <h1 className="page-heading">Your Account</h1>
+      <h1 className="page-heading">Hi, {currentUser?.displayName}</h1>
       <div className={css["account-container"]}>
         <ul className={css["account-menu"]}>
           <li className={css["account-menu-item"]}>Orders</li>
           <li className={css["account-menu-item"] + " " + css["settings"]}>
-            Settings
+            My Reviews
           </li>
           <li
             onClick={signOutHandler}
@@ -51,71 +37,9 @@ const Account = () => {
             Sign Out
           </li>
         </ul>
+
         <div className={css["account-content"]}>
-          <div className={css["order-table"]}>
-            <div className={css["order-table-header"]}>
-              <span>Order #</span>
-              <span>Total</span>
-              <span>Order Date</span>
-              <span>Items</span>
-              <span>Details</span>
-            </div>
-
-            {purchases &&
-              purchases.map((order, index) => {
-                const isSelected = selectedOrderIndex == index;
-
-                return (
-                  <Fragment key={index}>
-                    <div
-                      onClick={() => toggleDescription(index)}
-                      className={css["order-table-info"]}
-                    >
-                      <span># {order.id.slice(14).toUpperCase()}</span>
-                      <span>${order.total}</span>
-                      <span>
-                        {new Date(order.createdAt).toLocaleDateString()}
-                      </span>
-                      <span>{order.products.length} items</span>
-                      <span>{isSelected ? "↑" : "↓"}</span>
-                    </div>
-
-                    <div
-                      className={`${css["order-details"]} ${
-                        isSelected ? css["show-details"] : ""
-                      }`}
-                    >
-                      {order.products.map((product, index) => (
-                        <div key={index} className={css["order-item"]}>
-                          <img
-                            src={product.thumbnail}
-                            alt="item"
-                            className={css["order-item-thumbnail"]}
-                          />
-                          <div
-                            onClick={() => navigate(`/product/${product.id}`)}
-                            className={css["order-item-details"]}
-                          >
-                            <h3 className={css["order-item-title"]}>
-                              {product.name} <br />
-                              <span className={css["order-item-brand"]}>
-                                {product.brand}
-                              </span>
-                            </h3>
-                            <p>Quantity: {product.quantity}</p>
-                          </div>
-                          <div className={css["order-item-price"]}>
-                            <span>${product.price}</span>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </Fragment>
-                );
-              })}
-          </div>
-
-          {/* We're starting to think you're secretly a skincare expert - you haven't ordered anything from us yet! */}
+          <OrderHistory />
         </div>
       </div>
     </>
