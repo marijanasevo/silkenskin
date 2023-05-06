7c266d824f5b493510a1dd27ebb5a143570a9ae5
Maryen <voujum@gmail.com>
2023-05-06 10:43:10 +0200

    Added Image Zoom for product side images

--- src/components/product-side-images/product-side-images.component.tsx
+++ src/components/product-side-images/product-side-images.component.tsx
@@ -1,4 +1,8 @@
+import InnerImageZoom from "react-inner-image-zoom";
+
 import { CategoryItem } from "../../store/category/category.types";
+
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.min.css";
 import css from "./product-side-images.module.css";
 
 type ProductSideImagesProps = {
@@ -9,11 +13,11 @@ const ProductSideImages = ({ product }: ProductSideImagesProps) => {
   return (
     <div className={css["gallery-container"]}>
       {product?.gallery.map((imageSource, i) => (
-        <img
+        <InnerImageZoom
           className={css["gallery-image"]}
           key={i}
           src={imageSource}
-          alt={`${product?.name} image ${i}`}
+          imgAttributes={{ alt: `${product?.name} image ${i}` }}
         />
       ))}
     </div>
